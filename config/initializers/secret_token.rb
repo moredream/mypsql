7720a08853fe244c500c42ea861b03af4b2858d5
moredream <moredream@moredream-virtual-machine.(none)>
2012-08-08 17:27:01 +0900

    First Init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mypsql::Application.config.secret_token = '9dd057cbb1bd0869670d6f653ecf346df520a16339832bf7288d3793af92380d822b6fb56628d73faa2594cbb013eca6189fb6c4fe3a795d28475521836760c4'
