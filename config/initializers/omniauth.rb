d7f931bd738957773a7a1123db2a9c7545992634
moredream <moredream@moredream-virtual-machine.(none)>
2012-08-18 21:36:32 +0900

    Bootstrap

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -3,7 +3,7 @@ OmniAuth.config.logger = Rails.logger
 ENV['FACEBOOK_APP_ID'] = "387404467992646"
 ENV['FACEBOOK_SECRET'] ="d8f8ccf870edb9b37a882178e59ee5cd"
 
-Rails.application.config.middleware.use OmniAuth::Builder do
- provider :facebook, ENV['FACEBOOK_APP_ID'] , ENV['FACEBOOK_SECRET'] 
-end
 
+Rails.application.config.middleware.use OmniAuth::Builder do
+  provider :facebook, ENV['FACEBOOK_APP_ID'], ENV['FACEBOOK_SECRET'], scope: "email,publish_stream"
+end
