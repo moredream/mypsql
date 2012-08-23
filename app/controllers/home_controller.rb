938b9e064a9fbef231b161372ab8d3c5eed7beed
moredream <moredream@moredream-virtual-machine.(none)>
2012-08-23 09:34:02 +0900

    Login fix

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,11 @@
 class HomeController < ApplicationController
   def index
+	begin
+		@graph = Koala::Facebook::API.new(facebook_cookies["access_token"])
+		@profile = @graph.get_object("me")
+	rescue
+	  nil # not logged in
+	end
   end
 
   def show
--- app/controllers/paintings_controller.rb
+++ app/controllers/paintings_controller.rb
@@ -22,14 +22,25 @@ class PaintingsController < ApplicationController
   def publish
 
       @painting = Painting.find(params[:id])
-      # graph = Koala::Facebook::GraphAPI.new(facebook_cookies["access_token"])
-      # if graph
-      #   graph.put_picture(@painting.image.path,{:message => "Make my ColorCode"})
-      # end
+         # begin
+    #   # allow for ?access_token=[TOKEN] for iOS calls.
+    #   @access_token = session[:access_token] || facebook_cookies[:access_token]
+    #   @graph = Koala::Facebook::API.new(@access_token)
+    #   # TODO: move this to session[:current_user]..
+    #   @current_user ||= User.from_graph @graph.get_object('me', { fields: 'id,first_name,last_name,gender,birthday' })
+    # rescue
+    #   nil # not logged in
+    # end
+        graph = Koala::Facebook::API.new(facebook_cookies["access_token"])
       
-      if current_user
-        current_user.facebook.put_picture(@painting.image.path,{:message => "Make my ColorCode"})
+      if graph
+        graph.put_picture(@painting.image.path,{:message => "Make my ColorCode"})
       end
+      
+
+      # if current_user
+      #   current_user.facebook.put_picture(@painting.image.path,{:message => "Make my ColorCode"})
+      # end
 
     redirect_to  home_show_path, notice: "Review has been created."
 
